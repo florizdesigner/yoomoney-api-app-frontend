3a8f2c5daef657e24b7160a23cd64c4f6ea03036
florizdesigner <vyacheslav.sokolovv@icloud.com>
2023-02-23 00:25:28 +0300

    create store and auth form

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { observer } from 'mobx-react-lite';
+import React, {FC, useContext, useEffect, useState} from 'react';
+import LoginForm from "./components/LoginForm";
+import {Context} from "./index";
+import {IUser} from "./models/IUser";
+import UserService from "./services/UserService";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: FC = () => {
+    const {store} = useContext(Context)
+    const [users, setUsers] = useState<IUser[]>([])
 
-export default App;
+    useEffect(() => {
+        setUsers([])
+        if(localStorage.getItem('token')) store.checkAuth()
+    }, [])
+
+
+    async function getUsers () {
+        try {
+            const response = await UserService.fetchUsers()
+            setUsers(response.data)
+        } catch (e) {
+            console.log(e)
+        }
+    }
+
+    if(store.isLoading) return <div>Loading...</div>
+    if(!store.isAuth) return <LoginForm/>
+
+        return (
+            <div>
+                <h1>{store.isAuth ? `User is authorized ${store.user.email}` : "Авторизуйтесь!"}</h1>
+            <h1>{store.user.isActivated ? "Account is activated" : "Warning! Account isn't activated"}</h1>
+                <button onClick={() => store.logout()}>Logout</button>
+                <div>
+                    <button onClick={getUsers}>Get clients</button>
+                </div>
+                {users.map(user => <div key={user.email}>{user.email}</div>)}
+            </div>
+        );
+};
+
+export default observer(App)
